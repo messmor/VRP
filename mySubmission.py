8698797480fcfeac77623c9f18dfcd4551933d48
mitchell27 <mitchell@inseer.com>
2023-11-24 13:49:06 -0600

    "Fix submission scripts for challenge...I forgot to change to the lowest loss solver. "

--- mySubmission.py
+++ mySubmission.py
@@ -1,5 +1,5 @@
 import argparse
-from Solutions.sweepSolution import sweepWith2OptVRP
+from Solutions.greedySolution import greedyBestStartVRP
 
 
 if __name__ == "__main__":
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     parser.add_argument("file_path", type=str, help="The file path to the VRP problem .txt file.")
     args = parser.parse_args()
     # run VRP solution
-    schedules = sweepWith2OptVRP(args.file_path)
+    schedules = greedyBestStartVRP(args.file_path)
     # print out routes to match submission guidelines
     for route in schedules:
         print([int(x) for x in route])
